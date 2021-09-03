f6e2add71b51df1ca45e7f51a9f47ec765369a25
Heloiza Paulichen <helo.paulichen@gmail.com>
2021-09-03 10:17:12 -0400

    estudos de csharp

---
+++ Classes/Produto.cs
@@ -0,0 +1,62 @@
+﻿using System.Globalization;
+
+namespace Classes {
+    class Produto {
+
+        private string _nome;
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        public Produto() { }
+
+        public Produto(string nome, double preco, int quantidade) {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        /*public Produto(string nome, double preco) {
+            Nome = nome;
+            Preco = preco;
+        }*/
+
+        public string Nome {
+            get { return _nome; }
+            set {
+                if (value != null && value.Length > 1)
+                    _nome = value;
+            }
+        }
+
+        /*public string GetNome() {
+            return _nome;
+        }
+
+        public void SetNome(string nome) {
+            if(nome != null && nome.Length > 1)
+                _nome = nome;
+        }*/
+
+        public double ValorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int qtd) {
+            Quantidade += qtd;
+        }
+
+        public void RemoverProdutos(int qtd) {
+            Quantidade -= qtd;
+        }
+
+        public override string ToString() {
+            return _nome
+                + ", R$ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: R$ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Classes {
+    class Program {
+        static void Main(string[] args) {
+
+            //ExecutaTriangulo();
+            //ExecutaProduto();
+            //ExecutaCalculadora();
+            //ExecutaContaBancaria();
+            //ExecutaNullables();
+            //ExecutaLista();
+            ExecutaMatriz();
+
+        }
+
+        private static void ExecutaMatriz() {
+
+            int n = int.Parse(Console.ReadLine());
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++) {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++) {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.WriteLine("Diagonal: ");
+            for (int i = 0; i < n; i++) {
+                Console.Write(mat[i,i] + " ");
+            }
+            Console.WriteLine();
+
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (mat[i, j] < 0)
+                        count++;
+                }
+            }
+            Console.WriteLine("Negativos: " + count);
+        }
+
+        private static void ExecutaLista() {
+            List<string> list = new List<string>();
+
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Anna");
+            list.Insert(2, "Marco");
+
+            foreach (string obj in list)
+                Console.WriteLine(obj);
+
+            Console.WriteLine("List count: " + list.Count);
+            string s1 = list.Find(x => x[0] == 'A');
+            string s2 = list.FindLast(x => x[0] == 'A');
+
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+        }
+
+        private static void ExecutaNullables() {
+            
+            double? x = null;
+            double? y = 10.0;
+
+            double a = x ?? 5;
+            double b = y ?? 4;
+
+            if (!x.HasValue)
+                Console.WriteLine("x is null");
+        }
+
+        private static void ExecutaContaBancaria() {
+
+            ContaBancaria conta;
+
+            Console.Write("Entre o numero da conta: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string t = Console.ReadLine();
+            Console.Write("Haverá depósito inicial? (s/n) ");
+            char d = char.Parse(Console.ReadLine());
+            if(d == 's') {
+                Console.Write("Digite o Valor do depósito: ");
+                double s = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(n, t, s);
+            } else {
+                conta = new ContaBancaria(n, t);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para deposito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+
+        }
+
+        public static void ExecutaProduto() {
+
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do Produto: " + p);
+            
+            Console.WriteLine();
+            Console.Write("Digite o numero de itens a serem adicionados ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o numero de itens a serem removidos ao estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+        }
+
+        public static void ExecutaTriangulo() {
+
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Digite as medidas do Triangulo X:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite as medidas do Triangulo Y:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+                Console.WriteLine("Maior área: X");
+            else
+                Console.WriteLine("Maior área: Y");
+        }
+
+        public static void ExecutaCalculadora() {
+
+            Console.WriteLine("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double vol = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + vol.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("valor de Pi: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+
+    }
+}
