602cbb38606e5643e925a93c7d44b42567b39338
Heloiza Paulichen <helo.paulichen@gmail.com>
2021-09-17 12:49:52 -0400

    atividade com linq

---
+++ EstudosLINQ/EstudosLINQ/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace EstudosLINQ.Entities {
+    class Product {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public override string ToString() {
+            return Id + ", " + Name + ", " + Price.ToString("F2", CultureInfo.InstalledUICulture) + ", " + Category.Name + ", " + Category.Tier;
+        }
+    }
+
+}
